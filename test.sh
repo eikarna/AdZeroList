3247ed635a69a16df5ae0035fd6eed8ebd49d176
eikarna <nixic0@proton.me>
2025-07-20 20:20:29 +0700

    fix: Ensure newline before adding custom domains

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 while IFS= read -r domain; do
   # Pastikan baris tidak kosong dan bukan komentar
   if [[ -n "$domain" && ! "$domain" =~ ^\s*# ]]; then
     echo "0.0.0.0 $domain" >> hosts.raw
   fi
 done < custom.list
+
